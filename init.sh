615a7fa491ffc19112123ea80fa38410e1003d44
raylexlee <raylexym57lee@gmail.com>
2021-02-07 10:02:45 +0800

    Finalize init.sh

--- init.sh
+++ init.sh
@@ -23,7 +23,20 @@ grep -e 'id=[0-9]*">.*</br>' ../book.html | sed 's/.*id=\([0-9]*\)">\([^<]*\)<[^
 MISSING_XCANGJIE_IN_XTEXT=$(node ../checkxfonts.js)
 if [[ -n $MISSING_XCANGJIE_IN_XTEXT ]]; then
    echo $MISSING_XCANGJIE_IN_XTEXT
+   cp ../xtext.html index.html
+   cp ../xtext.js .
+   live-server
    exit 1
 fi   
 echo $(node ../gen2spx.js) > last_chapter.txt
 ../all_make_cover 001
+cd audio
+cp $HOME/my-ms-translator/officialdom/temp/key .
+cp $HOME/my-ms-translator/officialdom/temp/region .
+../../all_make_audio 001
+rm *wav
+rm *log
+rm key
+rm region
+cd ..
+../all_make_youtube 001
